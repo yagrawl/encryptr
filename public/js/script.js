0cb706d2dc863725cb871cd4c70ccfa8f81ddc7a
Yash Agrawal <yagrawl2@gmail.com>
2018-04-12 21:55:54 -0500

    encryption next

---
+++ public/js/script.js
@@ -0,0 +1,11 @@
+function encrypt(text, key) {
+    let output = document.getElementById("encryptedtext");
+    console.log(text)
+    output.textContent = text;
+}
+
+function decrypt(text, key) {
+    let output = document.getElementById("decryptedtext");
+    console.log(text)
+    output.textContent = text;
+}
