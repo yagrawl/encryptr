f39a8d7f9d3ff6f56b4813faa143cb539f03c94a
Yash Agrawal <yagrawl2@gmail.com>
2018-04-13 04:33:41 -0500

    bug ridden. barely working version

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,34 @@
 function encrypt(text, key) {
     let output = document.getElementById("encryptedtext");
-    console.log(text)
-    output.textContent = text;
+    let adder = 0
+    let encryptedText = '';
+    for(let i = 0; i < key.length; i++) {
+        adder += key.charCodeAt(i);
+    }
+    adder %= 128;
+
+    for(let i = 0; i < text.length; i++) {
+        if(text.charCodeAt(i) + adder < 128) {
+            encryptedText += String.fromCharCode(text.charCodeAt(i) + adder);
+        }
+        else {
+            encryptedText += String.fromCharCode(text.charCodeAt(i) + adder - 128);
+        }
+    }
+    output.textContent = encryptedText;
 }
 
 function decrypt(text, key) {
     let output = document.getElementById("decryptedtext");
-    console.log(text)
-    output.textContent = text;
+    let subtracter = 0;
+    let decryptedText = '';
+    for(let i = 0; i < key.length; i++) {
+        subtracter += key.charCodeAt(i);
+    }
+    subtracter %= 128;
+
+    for(let i = 0; i < text.length; i++) {
+        decryptedText += String.fromCharCode(text.charCodeAt(i) - subtracter);
+    }
+    output.textContent = decryptedText;
 }
