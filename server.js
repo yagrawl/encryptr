917bd70cd9437a40ee94cfe1d87c25c25fc1838b
Yash Agrawal <yagrawl2@gmail.com>
2018-04-12 19:28:20 -0500

    ejs template added

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.send('This is working, bitch.');
+    res.render('index', {data:''});
 });
 
 app.listen(PORT, () => {
